380738421d61814df9f9918aa6e60afb5b1874a6
chloevent <Chloe.Viriyapunt@gmail.com>
2017-05-10 11:17:24 -0700

    just to practice

--- TemperatureConverter/TemperatureConverter/TemperatureForm.Designer.cs
+++ TemperatureConverter/TemperatureConverter/TemperatureForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TemperatureConverter
 {
-    partial class Form1
+    partial class TemperatureForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TemperatureForm));
             this.convertButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
             this.tempLabel = new System.Windows.Forms.Label();
